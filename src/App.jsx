755bb1342cde5745fbbe8bd3cb75fe26189a1a88
Grabus61 <bugragedikli024@gmail.com>
2024-12-31 05:20:51 +0300

    Router basename eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="bg-primary min-h-screen text-white">
         <Navbar />
         <Routes>
